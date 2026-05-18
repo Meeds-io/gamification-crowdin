4acc38fc413b1eb901d7d9968cf8414c6ac16680
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2026-05-18 10:53:57 +0100

    feat: Upgrade to Spring Boot 4.1 - Meeds-io/meeds#3889

--- gamification-crowdin-services/src/test/java/io/meeds/crowdin/gamification/service/WebhookServiceTest.java
+++ gamification-crowdin-services/src/test/java/io/meeds/crowdin/gamification/service/WebhookServiceTest.java
@@ -39,7 +39,7 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.Arrays;
 import java.util.List;
@@ -51,10 +51,10 @@ class WebhookServiceTest {
 
   private static final String    USER       = "user";
 
-  @MockBean
+  @MockitoBean
   private CrowdinConsumerStorage crowdinConsumerStorage;
 
-  @MockBean
+  @MockitoBean
   private WebHookStorage         webHookStorage;
 
   @Autowired
